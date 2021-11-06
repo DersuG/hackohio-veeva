9553a8830d6eed8ae26e10acd639c20d1e0bd16d
DersuG <dersu.goorskey@gmail.com>
2021-11-06 11:58:35 -0400

    Added pandas for parsing .csv files

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import streamlit
-import pandas
+#import streamlit as sl
+import pandas as pd
+
+# Load data:
+data_df = pd.read_csv('Prescriber_Data.csv')
+print(f'{data_df}')
+
+print(f'{data_df["id"]}')
+
+
+
+
 
