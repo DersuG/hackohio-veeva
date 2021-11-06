d3529baa086e80f74cedc664a21734def5e43064
tobiasSimpson <tobias.simpson0@gmail.com>
2021-11-06 18:48:29 -0400

    Added checkboxes for products

--- data.py
+++ data.py
@@ -66,6 +66,7 @@ def TRxByProduct(data):
     TRx5 = 0
     TRx6 = 0
     productsData = pd.DataFrame()
+    sl.header('Total Global Prescriptions')
     for x in range(len(data.index)):
         if(x>0):
             if(data.iloc[x,4] != data.iloc[x-1,4]):
@@ -77,14 +78,18 @@ def TRxByProduct(data):
                     TRx4+=row['TRx_Month_4']
                     TRx5+=row['TRx_Month_5']
                     TRx6+=row['TRx_Month_6']
-                sl.header('Total Global Prescriptions for '+dataProduct.iloc[0,4])
-                col1,col2,col3,col4,col5,col6 = sl.columns(6)
-                col1.metric('Month 1',TRx1)
-                col2.metric('Month 2',TRx2,TRx2-TRx1)
-                col3.metric('Month 3',TRx3,TRx3-TRx2)
-                col4.metric('Month 4',TRx4,TRx4-TRx3)
-                col5.metric('Month 5',TRx5,TRx5-TRx4)
-                col6.metric('Month 6',TRx6,TRx6-TRx5)
-                productsData = pd.concat([productsData,pd.DataFrame({dataProduct.iloc[0,4]:[TRx1,TRx2,TRx3,TRx4,TRx5,TRx6]})],axis=1)
+                head1,head2 = sl.columns(2)
+                head1.header(dataProduct.iloc[0,4])
+                head2=head2.checkbox(label=dataProduct.iloc[0,4],value=True)
+                if head2:
+                    col1,col2,col3,col4,col5,col6 = sl.columns(6)
+                    col1.metric('Month 1',TRx1)
+                    col2.metric('Month 2',TRx2,TRx2-TRx1)
+                    col3.metric('Month 3',TRx3,TRx3-TRx2)
+                    col4.metric('Month 4',TRx4,TRx4-TRx3)
+                    col5.metric('Month 5',TRx5,TRx5-TRx4)
+                    col6.metric('Month 6',TRx6,TRx6-TRx5)
+                    productsData = pd.concat([productsData,pd.DataFrame({dataProduct.iloc[0,4]:[TRx1,TRx2,TRx3,TRx4,TRx5,TRx6]})],axis=1)
                 start = x
-    sl.line_chart(productsData)
+    if productsData.size>0:   
+        sl.line_chart(productsData)
